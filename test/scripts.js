109a4ad65e0450e7869e24471b5fdd2ace257d00
Gabriela Iaccovazzo de Almeida Cantarini <gabilana64@hotmail.com>
2023-02-13 15:57:01 +0000

    feature container

---
+++ test/scripts.js
@@ -0,0 +1,26 @@
+function calcular(n1,n2){
+    n1 = parseFloat (document.getElementById("n1").value)
+    n2 = parseFloat (document.getElementById("n2").value)
+
+    Selector = document.getElementById("selector").value
+
+    switch(selector){
+        case '+':
+            calculo = (n1 + n2) 
+            document.getElementById("resultado").innerHTML = "O resultado é" = calculo
+            break
+
+        case '-':
+            calculo = (n1 - n2) 
+            document.getElementById("resultado").innerHTML `o resultado da subtração de ${n1} - ${n2} é = ${calculo}`
+            break
+
+        case '/':
+            calculo = Math.round((n1 / n2))
+            if (Number.isNaN(calculo)){
+                document.getElementById("resultado").innerHTML = 'insira um divisivel valido'
+            }else{
+            document.getElementById("resultado").innerHTML `o resultado da divisão de ${n1} / ${n2} é = ${calculo}`
+            }
+    }
+}   
