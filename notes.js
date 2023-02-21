1e88cee0f23dbb69aaa29da466e5a91ef7a60749
Gabriela Iaccovazzo de Almeida Cantarini <gabilana64@hotmail.com>
2023-02-21 11:55:53 +0000

    added bmi calculator function

--- notes.js
+++ notes.js
@@ -40,3 +40,16 @@ alert("These are the results of the calculations by Dr. Love: "
 )
 
 
+
+function bmiCalculator(weight, height) {
+    var bmi = Math.floor(weight / Math.pow(height, 2));
+
+    if(bmi < 18.5) {
+        return("Your BMI is " + bmi + " , so you are underweight.");
+    } if (bmi > 18.5 && bmi < 24.9) {
+        return("Your BMI is " + bmi + " , so you are normal weight.");
+    } else {
+        return("Your BMI is " + bmi + " , so you are overweight.");
+    }
+}
+bmiCalculator(100, 1);
