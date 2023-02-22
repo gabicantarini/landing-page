6bbcf68c4de8510e543cab600bbb176f8ddbf6d6
Gabriela Iaccovazzo de Almeida Cantarini <gabilana64@hotmail.com>
2023-02-22 11:04:16 +0000

    feature background img

--- drumKit/js/index.js
+++ drumKit/js/index.js
@@ -1,9 +1,9 @@
 
 var numberOfDrumButtons = document.querySelectorAll(".drum").length;
-
+var audio = new Audio('./sounds/crash.mp3');
 for (var i = 0; i<numberOfDrumButtons; i++) {
     document.querySelectorAll(".drum")[i].addEventListener("click", function() {   
-        alert("I got clicked!")
+        audio.play();
     });
 }
 
