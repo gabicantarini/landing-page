d04698100bca68204d2153d9c1d4a14185d72c75
Gabriela Iaccovazzo de Almeida Cantarini <gabilana64@hotmail.com>
2023-02-23 09:15:24 +0000

    feature note

--- drumKit/js/index.js
+++ drumKit/js/index.js
@@ -1,7 +1,9 @@
 
+
 var numberOfDrumButtons = document.querySelectorAll(".drum").length;
 
 for (var i = 0; i < numberOfDrumButtons; i++) {
+    //get drums click
     document.querySelectorAll(".drum")[i].addEventListener("click", function () {
         
         //verify mouse click
