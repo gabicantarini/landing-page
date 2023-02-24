75dd7b8e91f7eed2d477362383ce53afd359f21d
Gabriela Iaccovazzo de Almeida Cantarini <gabilana64@hotmail.com>
2023-02-24 08:50:11 +0000

    feature playsound

--- simonGame/js/game.js
+++ simonGame/js/game.js
@@ -1,15 +1,39 @@
 
 var buttonCollors = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
+var userClickedPattern = [];
 
+$(".btn").click(function() {
+
+    var userChosenColour = $(this).attr("id");
+      
+    userClickedPattern.push(userChosenColour);
+    playSound(userChosenColour);
+    animatePress(userChosenColour);
+  
+});
 
 
 function nextSequence() {
     var randomNumber = Math.floor((Math.random() * 4));
     var randomChosenColour = buttonCollors[randomNumber];
     gamePattern.push(randomChosenColour);
+    playSound(randomChosenColour);
+    animatePress(randomChosenColour);
+
+ //1. Select the button with the same id as the randomChosenColour
+ $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+
+
+}
+
+
+function playSound(name) {
+    
+    var audio = new Audio("sounds/" + name + ".mp3");
+    audio.play();
+          
 }
 
 
 
-console.log(nextSequence());
