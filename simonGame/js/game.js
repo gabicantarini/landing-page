fdd9d239a94fd9ff4943f8f20e9b41938531f84b
Gabriela Iaccovazzo de Almeida Cantarini <gabilana64@hotmail.com>
2023-03-01 17:27:18 +0000

    feature homepage

--- simonGame/js/game.js
+++ simonGame/js/game.js
@@ -30,20 +30,23 @@ $(".btn").click(function() {
 
 function checkAnswer(currentLevel) {
    if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
-    console.log("success");
-
     if (userClickedPattern.length === gamePattern.length) {
-
         setTimeout( function() {
             nextSequence();
         }, 1000);
     }
-   }
-   else {
-    console.log("Wrong");    
+   } else {
+    playSound("wrong");
+      $("body").addClass("game-over");
+      $("#level-title").text("Game Over, Press Any Key to Restart");
+
+      setTimeout(function () {
+        $("body").removeClass("game-over");
+      }, 200);
+
+      startOver(); 
    }
 }
-
 
 
 function nextSequence() {
@@ -83,5 +86,9 @@ function animatePress(currentColour) {
     
 }
 
-
+function startOver() {
+    level = 0;
+    gamePattern = [];
+    started = false;
+  }
 
