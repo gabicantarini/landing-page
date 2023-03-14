caea43bf5b8503b55e0718bfc8ceee52af69e673
Gabriela Iaccovazzo de Almeida Cantarini <gabilana64@hotmail.com>
2023-03-14 15:56:02 +0000

    added calculator

--- express-server/server.js
+++ express-server/server.js
@@ -19,10 +19,6 @@ app.get("/about", function(rer, res){
     res.send("I am Gabriela. I am 37 years old.");
 })
 
-app.get("/hobbies", function(req, res){
-    res.send("<ul><li>Milk</li><li>Choccolat</li><li>gym</li></ul>")
-})
-
 app.listen(3000, function(){
     console.log("Server started at port 3000");
 });
