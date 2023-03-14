e61445f550d52e47068dfe243baa3f80b3aeb021
Gabriela Iaccovazzo de Almeida Cantarini <gabilana64@hotmail.com>
2023-03-14 15:03:31 +0000

    feature new get

--- express-server/server.js
+++ express-server/server.js
@@ -1,16 +1,21 @@
 //jshint esversion:6
 
 //new server building sintax
+const { response } = require("express");
 const express = require("express");
 
 const app = express();
 
 //req = request
 //res = response
-app.get("/", function(req, res) {
+app.get("/", function(request, response){
     response.send("<h1>Hello</h1>");
 });
 
-app.listen(3000, function() {
+/*app.get("/contact", function(req, res){
+    res.send("Contact: gabriela.cantarini@html.com");
+});*/
+
+app.listen(3000, function(){
     console.log("Server started at port 3000");
 });
