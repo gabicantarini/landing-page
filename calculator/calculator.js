da0db1b8317b3a902869487c7836005dbbd3cdfb
Gabriela Iaccovazzo de Almeida Cantarini <gabilana64@hotmail.com>
2023-03-14 17:09:00 +0000

    reinstall express

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -6,18 +6,24 @@ bodyParser.text
 bodyParser.urlencoded => to get index.html post information to my server
 */
 
+//jshint esversion:6
+
 const express = require("express");
+
 const bodyParser = require("body-parser");
 
 const app = express();
-app.use(bodyParser.urlencoded({extended: true}))
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function(req, res){
     res.sendFile(__dirname + "/index.html");
 });
 
 app.post("/", function(req, res){
-    console.log(req.body);
+
+console.log(req.body);
+
     res.send("LEGAL!");
 });
 
