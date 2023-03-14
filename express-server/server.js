89418c23791ad892392dbf957bd64f5aa29445c8
Gabriela Iaccovazzo de Almeida Cantarini <gabilana64@hotmail.com>
2023-03-14 14:32:58 +0000

    feature server test

--- express-server/server.js
+++ express-server/server.js
@@ -6,7 +6,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", function(request, response) {
-    console.log(request);
+    response.send("Hello");
 });
 
 app.listen(3000, function() {
