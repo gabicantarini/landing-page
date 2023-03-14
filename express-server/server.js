261d0e632c713e9f7e274a418b00eed44196986e
Gabriela Iaccovazzo de Almeida Cantarini <gabilana64@hotmail.com>
2023-03-14 15:30:53 +0000

    feature testing get

--- express-server/server.js
+++ express-server/server.js
@@ -1,20 +1,23 @@
 //jshint esversion:6
 
 //new server building sintax
-const { response } = require("express");
 const express = require("express");
 
 const app = express();
 
 //req = request
 //res = response
-app.get("/", function(request, response){
-    response.send("<h1>Hello</h1>");
+app.get("/", function(req, res){
+    res.send("<h1>Hello</h1>");
 });
 
-/*app.get("/contact", function(req, res){
+app.get("/contact", function(req, res){
     res.send("Contact: gabriela.cantarini@html.com");
-});*/
+});
+
+app.get("/about", function(rer, res){
+    res.send("I am Gabriela. I am 37 years old.");
+})
 
 app.listen(3000, function(){
     console.log("Server started at port 3000");
