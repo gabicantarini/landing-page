cf9f271b203ac734bd7e8795b6f3320883f00e26
Gabriela Iaccovazzo de Almeida Cantarini <gabilana64@hotmail.com>
2023-03-14 16:58:27 +0000

    added body-parser

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,16 +1,26 @@
 // __dirname = search de directory address at any device or server
+/* OTHER Body-Parse methods
+
+bodyParser.json
+bodyParser.text
+bodyParser.urlencoded => to get index.html post information to my server
+*/
 
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.get("/", function(req, res){
     res.sendFile(__dirname + "/index.html");
 });
 
 app.post("/", function(req, res){
+    console.log(req.body);
     res.send("LEGAL!");
 });
+
 
 app.listen(3000, function(){
     console.log("Listening portal 3000");
