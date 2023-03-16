6ede3984abbcd2e3c84c63bfd8a5a854c9e1d810
Gabriela Iaccovazzo de Almeida Cantarini <gabilana64@hotmail.com>
2023-03-16 22:18:10 +0000

    fixed  Celsius metric unit

--- weatherProject/app.js
+++ weatherProject/app.js
@@ -7,7 +7,7 @@ const app = express();
 
 app.get("/", function(req, res){
 
-    const url = "https://api.openweathermap.org/data/2.5/weather?q=London&appid=34acc4e89b63d7659564bbd63bf03c66"
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=London&appid=34acc4e89b63d7659564bbd63bf03c66&units=metric"
     
     https.get(url, function(response){
         console.log(response.statusCode);
