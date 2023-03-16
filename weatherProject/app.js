9364d3fc4fae3f264f1b40981fef3cbfec9b81c0
Gabriela Iaccovazzo de Almeida Cantarini <gabilana64@hotmail.com>
2023-03-16 22:15:29 +0000

    returning img element

--- weatherProject/app.js
+++ weatherProject/app.js
@@ -17,8 +17,11 @@ app.get("/", function(req, res){
            const weatherData =  JSON.parse(data)
            const temp = weatherData.main.temp;
            const weatherDescription = weatherData.weather[0].description;
+           const icon = weatherData.weather[0].icon;
+           const imageURL = "https://openweathermap.org/img/wn/" + icon + "@2x.png";
            res.write("<h1>The weather is currently " + weatherDescription + "  </h1>");
-           res.write("<h1>The temperature in London is " + temp + " degrees Ceclcius.<h1>")
+           res.write("<h1>The temperature in London is " + temp + " degrees Ceclcius.<h1>");
+           res.write("<img src=" + imageURL + ">");
            res.send(); 
            console.log(JSON.stringify(temp));
         });
