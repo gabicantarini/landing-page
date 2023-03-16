5bf9fb88224d47fa75ebfb0afb2ca6f360027c3a
Gabriela Iaccovazzo de Almeida Cantarini <gabilana64@hotmail.com>
2023-03-16 20:46:25 +0000

    feature https

--- weatherProject/app.js
+++ weatherProject/app.js
@@ -6,6 +6,11 @@ const app = express();
 
 
 app.get("/", function(req, res){
+
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=London&appid=34acc4e89b63d7659564bbd63bf03c66"
+    https.get(url, function(response){
+        console.log(response);
+    });
     
     res.send("Server is running");
 });
