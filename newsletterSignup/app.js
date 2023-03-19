0e50e3c7ed47f7435fac29b600604e6e940c5b6c
Gabriela Iaccovazzo de Almeida Cantarini <gabilana64@hotmail.com>
2023-03-19 20:19:23 +0000

    fixed get method and static files

--- newsletterSignup/app.js
+++ newsletterSignup/app.js
@@ -7,10 +7,13 @@ const request = require("request");
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public")); // refer to our static files
 
 app.get("/", function(req, res){
     res.sendFile(__dirname + "/signup.html");
 });
+
+
 
 app.listen(3000, function(){
     console.log("Server is running on port 3000");
