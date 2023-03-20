f789d8e46db9a7c84908e34857b48d77d57af631
Gabriela Iaccovazzo de Almeida Cantarini <gabilana64@hotmail.com>
2023-03-20 18:50:45 +0000

    added post method

--- newsletterSignup/app.js
+++ newsletterSignup/app.js
@@ -1,22 +1,57 @@
 
 const express = require("express");
-
+const https = require("https");
 const bodyParser = require("body-parser");
 const request = require("request");
 
 const app = express();
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true })); // get html body information
 app.use(express.static("public")); // refer to our static files
 
-app.get("/", function(req, res){
+app.get("/", function (req, res) {
     res.sendFile(__dirname + "/signup.html");
 });
 
+app.post("/", function (req, res) {
 
+    const firstName = req.body.firstName;
+    const lastName = req.body.lastName;
+    const email = req.body.email;
 
-app.listen(3000, function(){
+    const data = {
+        members: [
+            {
+                //id: "string",
+                email_address: email,                
+                status: "subscribed",                
+                merge_fields: {
+                    FNAME: firstName,
+                    LNAME: lastName
+                }
+            }
+        ]
+
+    };
+
+    const jsonData = JSON.stringify(data);
+
+    https.request(url, options, function(response){
+        
+    })
+
+    console.log(firstName, lastName, email);
+});
+
+app.listen(3000, function () {
     console.log("Server is running on port 3000");
 });
 
 
+//API KEY
+//59b04bbf69228c28d54485c56474ebbe-us21
+
+//list ID
+//fc788a18d5
+
+//const jsonData = JSON.stringify(data) => pass data as json format
