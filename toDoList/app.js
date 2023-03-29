0b1106f3546d3a8f36083d221e6ddd68b388bdf4
Gabriela Iaccovazzo de Almeida Cantarini <gabilana64@hotmail.com>
2023-03-29 17:16:34 +0100

    fixed css path

--- toDoList/app.js
+++ toDoList/app.js
@@ -6,6 +6,7 @@ const bodyParser = require("body-parser");
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
 
 app.set("view engine", "ejs");
 
