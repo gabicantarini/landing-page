c3707f70d543bfafc7c4672f7266ccb2f90ed194
Gabriela Iaccovazzo de Almeida Cantarini <gabilana64@hotmail.com>
2023-03-29 13:53:00 +0100

    added switch case

--- toDoList/app.js
+++ toDoList/app.js
@@ -5,16 +5,43 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
     const today = new Date();
+    const curDay = today.getDay();
+    const day = "";
 
-    if(today.getDay() === 6 || today.getDay() === 0) {
-        res.send("Yay it's the weekend!");
-    } else {
-        res.send("Boo! I have to work!");
-    }
+   switch (curDay) {
+    case 0:
+        day = "Sunday";        
+        break;
+    case 1:
+        day = "Monay";        
+        break;
+    case 2:
+        day = "Tuesday";        
+        break;
+    case 3:
+        day = "Wednesday";        
+        break;
+    case 4:
+        day = "Thursday";        
+        break;
+    case 5:
+        day = "Friday";        
+        break;
+    case 6:
+        day = "Saturday";        
+        break;   
+    default:
+        console.log("Error: current day is equal to: " + curDay);
+
+   }
+
+    res.render("views/lists", {kindOfDay: day});
 });
 
 
