737136b5b44ee46c32f30eeca0c8b4737f86f354
Gabriela Iaccovazzo de Almeida Cantarini <gabilana64@hotmail.com>
2023-04-01 23:54:51 +0100

    removed bodyParser

--- toDoList/app.js
+++ toDoList/app.js
@@ -8,7 +8,7 @@ const app = express();
 const tasks = [];
 const workTasks = [];
 
-app.use(bodyParser.urlencoded({extended: true}));
+//app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 app.set("view engine", "ejs");
