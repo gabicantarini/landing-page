f41a70e42a82754ba94a273f4e16e2b8e43f2117
Gabriela Iaccovazzo de Almeida Cantarini <gabilana64@hotmail.com>
2023-04-12 22:21:50 +0100

    return pokemon list

--- pokedex/src/app.js
+++ pokedex/src/app.js
@@ -1,12 +1,24 @@
 const fetchPokemon = () => {
 
-    const url = `https://pokeapi.co/api/v2/pokemon/25`;
+    const getPokemonUrl = id => `https://pokeapi.co/api/v2/pokemon/${id}`;
     
-    fetch(url) //bring url from api - return promise 
+    const pokemonPrmoises = [];
+
+    for (let i = 1; i <= 150; i++) {
+        pokemonPrmoises.push(fetch(getPokemonUrl(i)).then(response => response.json())) 
+    }
+
+
+    Promise.all(pokemonPrmoises) //return a promise after resolve all the pokemonPrmoises
+        .then( pokemons => {
+            console.log("pokemons: ", pokemons);
+        })
+
+    /*fetch(url) //bring url from api - return promise 
         .then(response => response.json()) //return json response promise
         .then(pokemon => {
         console.log(pokemon)
-    })
+    })*/
 }
 
 fetchPokemon();
