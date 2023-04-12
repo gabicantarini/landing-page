2f88c71743afa94627243d3f293487328cb50f18
Gabriela Iaccovazzo de Almeida Cantarini <gabilana64@hotmail.com>
2023-04-13 00:05:29 +0100

    refact pokemons generate html

--- pokedex/src/appRefactoring.js
+++ pokedex/src/appRefactoring.js
@@ -4,14 +4,14 @@ const getPokemonUrl = id => `https://pokeapi.co/api/v2/pokemon/${id}`;
 const generatePokemonPromises = () => Array(150).fill().map((_, index) =>
 fetch(getPokemonUrl(index + 1)).then(response => response.json()))
 
-const generateHTML = pokemons => pokemons.reduce((acumulator, pokemon) => {
-    const types = pokemon.types.map(typeInfo => typeInfo.type.name);
+const generateHTML = pokemons => pokemons.reduce((acumulator, { name, id, types }) => {
+    const elementTypes = types.map(typeInfo => typeInfo.type.name);
 
     acumulator += `
         <li class="card">
-        <img class="card-image ${types[0]}" alt="${pokemon.name}" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png"/>
-        <h2 class="card-title">${pokemon.id}. ${pokemon.name}</h2>
-        <p class="card-subtitle">${types.join(' | ')}</p>
+        <img class="card-image ${elementTypes[0]}" alt="${name}" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png"/>
+        <h2 class="card-title">${id}. ${name}</h2>
+        <p class="card-subtitle">${elementTypes.join(' | ')}</p>
         </li>`
     return acumulator;
 }, '')
