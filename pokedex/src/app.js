bb1266d79b126febd22569a317e35bb586f87987
Gabriela Iaccovazzo de Almeida Cantarini <gabilana64@hotmail.com>
2023-04-12 21:57:42 +0100

    added fetch pokemon function

--- pokedex/src/app.js
+++ pokedex/src/app.js
@@ -1,1 +1,12 @@
-alert("pokedex test!");
+const fetchPokemon = () => {
+
+    const url = `https://pokeapi.co/api/v2/pokemon/25`;
+    
+    fetch(url) //bring url from api - return promise 
+        .then(response => response.json()) //return json response promise
+        .then(pokemon => {
+        console.log(pokemon)
+    })
+}
+
+fetchPokemon();
