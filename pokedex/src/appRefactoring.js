98c2a39e85fd14562edd11da2b661fa9bd7b611d
Gabriela Iaccovazzo de Almeida Cantarini <gabilana64@hotmail.com>
2023-04-12 23:51:49 +0100

    refact generateHTML const

--- pokedex/src/appRefactoring.js
+++ pokedex/src/appRefactoring.js
@@ -4,19 +4,18 @@ const getPokemonUrl = id => `https://pokeapi.co/api/v2/pokemon/${id}`;
 const generatePokemonPromises = () => Array(150).fill().map((_, index) =>
 fetch(getPokemonUrl(index + 1)).then(response => response.json()))
 
-const generateHTML = pokemons => {
-    return pokemons.reduce((acumulator, pokemon) => {
-       const types =  pokemon.types.map(typeInfo => typeInfo.type.name);
-        
-        acumulator += `
+const generateHTML = pokemons => pokemons.reduce((acumulator, pokemon) => {
+    const types = pokemon.types.map(typeInfo => typeInfo.type.name);
+
+    acumulator += `
         <li class="card">
         <img class="card-image ${types[0]}" alt="${pokemon.name}" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png"/>
         <h2 class="card-title">${pokemon.id}. ${pokemon.name}</h2>
         <p class="card-subtitle">${types.join(' | ')}</p>
         </li>`
-        return acumulator;
-    }, '')
-}
+    return acumulator;
+}, '')
+
 
 const insertPokemonsIntoPage = pokemons => {
     const ul = document.querySelector('[data-js="pokedex"]');
